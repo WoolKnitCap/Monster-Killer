6811ecf5de77a1c920ad8024d017d6ab15410ac3
WoolKnitCap <woolknitcap@outlook.com>
2020-09-04 20:49:19 -0400

    Added heal function.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,26 @@
 const ATK = 10;
 const CRIT_ATK = ATK * 2.5;
 const MONSTER_ATK = 20;
+const LAY_HANDS = 20;
 
 let maxLife = 250;
 let currentMonsterHealth = maxLife * 2;
 let currentPlayerHealth = maxLife;
 
 adjustHealthBars(maxLife);
+
+function endRound() {
+  const playerDamage = dealPlayerDamage(MONSTER_ATK);
+  currentPlayerHealth -= playerDamage;
+  setPlayerHealth(currentPlayerHealth);
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You Won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You Lost!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('You both lost.');
+  }
+}
 
 function attackMonster(mode) {
   let maxDamage;
@@ -17,15 +31,7 @@ function attackMonster(mode) {
   }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATK);
-  currentPlayerHealth -= playerDamage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You Won!');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('You Lost!');
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-    alert('You both lost.');
-  }
+  endRound();
 }
 
 function attackHandler() {
@@ -36,5 +42,12 @@ function strongAttackHandler() {
   attackMonster('CRIT ATK');
 }
 
+function healPlayerHandler() {
+  increasePlayerHealth(LAY_HANDS);
+  currentPlayerHealth += LAY_HANDS;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
